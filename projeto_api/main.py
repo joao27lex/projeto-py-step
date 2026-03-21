d18bb944cc3071a500153daac98a459753b72bee
João Linux <jplcampanella@gmail.com>
2026-03-21 16:16:02 -0300

    adiciona crud

--- projeto_api/main.py
+++ projeto_api/main.py
@@ -7,7 +7,7 @@ import app.database.db
 
 app = FastAPI()
 
-def get_connection():
+def get_connection():   
     return sqlite3.connect("database.db")
 
 @app.get("/")
@@ -38,5 +38,51 @@ def criar_usuario(nome: str, idade: int):
     conn.commit()
     conn.close()
 
-    return {"msg": "Usuário criado"}
+    return {"msg": f"Usuário {nome} de idade {idade} criado"}
           
+@app.delete("/usuarios")
+def deletar_usuario(id: int):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM usuarios WHERE id = ?",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {"msg": f"Usuário {id} deletado"}
+
+@app.put("/usuarios")
+def atualizar_usuario(id: int, nome: str, idade: int):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE usuarios SET nome = ?, idade = ? WHERE id = ?",
+        (nome, idade, id)
+    )
+    conn.commit()
+    conn.close()
+
+    return {
+        "id" : id,
+        "nome" : nome,
+        "idade" : idade
+    }
+
+@app.get("/usuarios/{id}")
+def pegar_usuario(id: int):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE id = ?",
+        (id, )
+    )
+    dados = cursor.fetchone()
+
+    conn.close()
+
+    return dados
