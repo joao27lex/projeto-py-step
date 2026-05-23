e8eb7e6c102abf7f0364f5f4631f22b0ca3a8a31
Linux <jplcampanella@gmail.com>
2026-05-23 13:21:26 -0300

    ADD: MVT com Django

--- projeto_api/app/schemas/usuario_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UsuarioCreate(BaseModel):
-    email: str
-    senha: str
-
-class Login(BaseModel):
-    email: str
-    senha: str
--- projeto_api/app/views/usuario_view.py
+++
@@ -1,6 +0,0 @@
-def resposta_sucesso(msg, data=None):
-    return {
-        "status": "sucesso",
-        "mensagem": msg,
-        "dados": data
-    } 
