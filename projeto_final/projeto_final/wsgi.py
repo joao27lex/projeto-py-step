6cf35f14dce8e1587535215d71aa6d48c3a47185
Linux <jplcampanella@gmail.com>
2026-08-15 14:19:35 -0300

    fix: wsgi

--- projeto_final/projeto_final/wsgi.py
+++ projeto_final/projeto_final/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for projeto_final project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_final.settings')
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()
