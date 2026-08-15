a5dd0a89c9b42230f8421b514a66b08591866bdb
Linux <jplcampanella@gmail.com>
2026-08-15 15:07:04 -0300

    FIX: deploy ainda nao funciona³

--- projeto_final/projeto_final/wsgi.py
+++ projeto_final/projeto_final/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seu_projeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_final.settings')
 
 application = get_wsgi_application()
+app = application  
