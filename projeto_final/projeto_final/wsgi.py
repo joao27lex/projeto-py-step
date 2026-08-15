e840520b2f3ebe57d9995dc6582a5090242bd9aa
Linux <jplcampanella@gmail.com>
2026-08-15 14:45:03 -0300

    FIX: deploy no vercel ainda não funciona

--- projeto_final/projeto_final/wsgi.py
+++ projeto_final/projeto_final/wsgi.py
@@ -1,8 +1,6 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seu_projeto.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
