9cd1231f77eb139816727f34963c181d9eccaa3d
Linux <jplcampanella@gmail.com>
2026-08-15 15:18:02 -0300

    FIX: AllowedHosts

--- projeto_final/projeto_final/settings.py
+++ projeto_final/projeto_final/settings.py
@@ -24,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&k18w5*8(1dv8m13346br^@b80tfc35o9ns30g1ty^nazuxff5'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-&k18w5*8(1dv8m13346br^@b80tfc35o9ns30g1ty^nazuxff5')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
