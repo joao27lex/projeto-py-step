06ed189afaf8c599e5a0607847910ed2a9d9f2a4
Linux <jplcampanella@gmail.com>
2026-08-15 15:24:28 -0300

    FIX: deploy

---
+++ projeto_final/api/index.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.insert(
+    0,
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto_final.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
